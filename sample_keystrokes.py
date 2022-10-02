6b4eb1b14a9f2d23a00fe2c1a8ddf101eb4f271a
const-ae <artjom31415@googlemail.com>
2022-10-02 12:13:23 +0200

    Add example keystrokes

---
+++ sample_keystrokes.py
@@ -0,0 +1,4 @@
+keystrokes1 = "aaaabaaaabbaaaaaabaaaaabaaaaaaaaaabaab"
+keystrokes2 = "aabbaabbaabbaabbbabbaabbaabbaabbabbaab"
+keystrokes3 = "aababaaababaaababaaababaaababaaaababab"
+keystrokes4 = "aaababbabbbabbaabaaaabbbbbabbbaaababbab"
